b797a9ff157fbc367a2834b8657590baf6aab513
Mike Heft <mikeheft@gmail.com>
2020-04-16 14:49:56 -0600

    Add callbacks to inc/dec number of favs on video

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -5,6 +5,21 @@ class Favorite < ApplicationRecord
   validates :user_id, :video_id, presence: true
   validates :video_id, uniqueness: { scope: :user_id }
   
-  # TODO: Create callbacks to increment and decrement video number of favorites
+  before_commit :increment_number_of_favorites, on: :create
+  before_destroy :decrement_number_of_favorites
+  
+  private
+  
+  def increment_number_of_favorites
+    new_total = video.number_of_favorites + 1
+    
+    video.update(number_of_favorites: new_total)
+  end
+  
+  def decrement_number_of_favorites
+    new_total = video.number_of_favorites - 1
+    
+    video.update(number_of_favorites: new_total)
+  end
   
 end
