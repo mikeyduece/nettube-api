df3d1d68add83ba36c6693d3bc87b6500491d3f5
Mike Heft <mikeheft@gmail.com>
2020-04-16 07:08:39 -0600

    Add route for user creation

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,10 @@ Rails.application.routes.draw do
   devise_for :admins
   devise_for :users
   root to: 'home#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    namespace :v1 do
+      resources :users, module: :users, only: %i[create]
+    end
+  end
 end
