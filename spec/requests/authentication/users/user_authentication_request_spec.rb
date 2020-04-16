50bd4c94a8b56c4131585e8335f3148a2d7aba5f
Mike Heft <mikeheft@gmail.com>
2020-04-16 07:03:13 -0600

    Add basic spec for user associations thus far

---
+++ spec/requests/authentication/users/user_authentication_request_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe 'User Authentication' do
+  it 'should create a User' do
+  
+  end
+
+  it 'should log a user in' do
+  
+  end
+
+  it 'should log a user out' do
+  
+  end
+end
