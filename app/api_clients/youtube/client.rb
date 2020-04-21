5a0f69f3363288e88d6588ec166f5b7674afa4bb
Mike Heft <mikeheft@gmail.com>
2020-04-21 14:11:22 -0600

    Add pagination for youtube client

--- app/api_clients/youtube/client.rb
+++ app/api_clients/youtube/client.rb
@@ -1,11 +1,25 @@
 class Youtube::Client
   
-  def search(term:)
-    get_url("?q=#{term}")
+  def search(term:, next_page: false, previous_page: false)
+    query = "?q=#{term}"
+    videos = get_url(query)
+    videos = search_with_pagination(videos, query, previous_page) if next_page
+    videos
   end
 
   private
-
+  
+  def set_page_tokens(video_return)
+    return video_return[:nextPageToken], video_return[:previousPageToken]
+  end
+  
+  def search_with_pagination(videos, query, previous_page)
+    next_page_token, previous_page_token = set_page_tokens(videos)
+    return get_url(query + "&pageToken=#{previous_page_token}") if previous_page
+    
+    get_url(query + "&pageToken=#{next_page_token}")
+  end
+  
   def get_url(url)
     response = connection.get(url)
     JSON.parse(response.body, symbolize_names: true)
